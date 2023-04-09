5c439ed6eff0d90136f8980ac26250a3b4b607b7
Merve Akın <m.akin-2001@hotmail.com>
2023-04-09 23:35:33 +0300

    Add files via upload

---
+++ kitap-main/OnlineMagaza-main/dagitik_sistemler/collections.js
@@ -0,0 +1,23 @@
+let COLLECTIONS = [
+    {
+        id: 1,
+        name: "Gece Yarısı Kütüphanesi",
+        price: "15TL",
+        img: "./images/1.jpg",
+        category: "Bilim kurgu"
+    },
+    {
+        id: 2,
+        name: "Fareler Ve İnsanlar",
+        price: "25TL",
+        img: "./images/2.jpg",
+        category: "Trajedi"
+    },
+    {
+        id: 3,
+        name: "Cesur Yeni Dünya",
+        price: "30TL",
+        img: "./images/3.png",
+        category: "Bilim kurgu"
+    },
+]
